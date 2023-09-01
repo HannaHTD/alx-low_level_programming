71cb411814784616c108d96e2511135416162f0e
HannaHTD <hanna.hanna.tamirat@gmail.com>
2023-09-01 11:53:26 -0700

    update

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+
+_putchar - Writes the character 'c' to the standard output (stdout).
+@c: The character to be printed.
+Return: On success, returns 1.
+Copy
+    On error, returns -1 and sets errno appropriately.
+*/
+int _putchar(char c)
+{
+return write(1, &c, 1);
+}
+
